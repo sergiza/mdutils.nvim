ecba4c997b19b33da52c91cd7ac24c6e1f68f3f2
sergiza <sergiza1994@gmail.com>
2026-03-27 01:35:06 +0100

    back to init.lua and require...

--- lua/mdutils/init.lua
+++ lua/mdutils/init.lua
@@ -1,3 +1,23 @@
 local M = {}
-function M.setup() end
+
+function M.setup()
+    vim.api.nvim_create_user_command("Mdutils", function(opts)
+        local arg = opts.args
+        if arg == "opener" then
+            require("mdutils.opener").run()
+        elseif arg == "todo" then
+            require("mdutils.todo").run()
+        elseif arg == "mediaplayer" then
+            require("mdutils.mediaplayer").run()
+        else
+            vim.notify("Mdutils: unknown command '" .. arg .. "'", vim.log.levels.ERROR)
+        end
+    end, {
+        nargs = 1,
+        complete = function()
+            return { "opener", "todo", "mediaplayer" }
+        end,
+    })
+end
+
 return M
